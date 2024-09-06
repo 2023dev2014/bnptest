261e2b412981af6c9fceaffcc248ed16e4e7e30f
BNP Test <bnp.dev2.014@gmail.com>
2024-09-06 18:11:43 +0200

    Base app struct

---
+++ BnpTest/BnpTest/BnpTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BnpTestApp.swift
+//  BnpTest
+//
+//  Created by BNP Test on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BnpTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
